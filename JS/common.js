6adb717839970412ea2467b9c8453f7e89342af2
kim joy <didlshakdi@gmail.com>
2023-03-31 01:41:17 +0900

    테마미리보기

---
+++ JS/common.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  //옵션 선택
+  $('.layout_option').slick({
+    slide: 'li',
+    arrows: false,
+    variableWidth: true,
+    infinite: false,
+    focusOnSelect: true,
+    swipeToSlide: true,
+  });
+
+  $('.layout_option').on('afterChange', function(){
+    $('.slick-slide').removeClass('on');
+    $(this).find('.slick-current').addClass('on');
+  });
+
+
+  //하루일기 이모지 
+  $('.sticker_emoji').slick({
+    slide: 'li',
+    arrows: false,
+    variableWidth: true,
+    infinite: false,
+    focusOnSelect: true,
+    swipeToSlide: true,
+  });
+
+  $('.sticker_emoji').on('afterChange', function(){
+    $('.slick-slide').removeClass('on');
+    $(this).find('.slick-current').addClass('on');
+  });
+
+
+  
+}); //End
